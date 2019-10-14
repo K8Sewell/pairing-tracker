c0cbe37af9408ba3bbc5c39ce2a998cddfb0a258
K8Sewell <kait@kaitsewell.com>
2019-10-14 12:07:43 -0700

    adds starter layout

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome, userName!</h1>
+        <p>Enjoy your customizable pair and mob programming tracker.</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
